518a19fb20fc5259787f2c11ebab188d0621bd70
yahagikimuthi <yahagikimuth@gmail.com>
2026-07-05 13:01:40 +0900

    コンポーネントを横断し、求人をポストする関数を追加

--- include/strategies/orchestrator.hpp
+++ include/strategies/orchestrator.hpp
@@ -1,3 +1,14 @@
 #pragma once
 
-namespace orchestrator {}
+#include "components/common.hpp"
+#include "components/goods_supplier.hpp"
+#include "components/labor_demander.hpp"
+
+namespace orchestrator {
+void postLaborRequest(
+    const agent_index::Component&                indexComp,
+    goods_supplier::Component&                   goodsSupplier,
+    labor_demander::Component&                   laborDemander,
+    tbb::concurrent_vector<world::LaborRequest>& requestBox
+);
+}
---
+++ src/strategies/orchestrator.cpp
@@ -0,0 +1,19 @@
+#include "strategies/orchestrator.hpp"
+
+#include <tbb/concurrent_vector.h>
+
+#include "strategies/goods_supplier/level1.hpp"
+#include "strategies/labor_demander/level1.hpp"
+
+namespace orchestrator {
+void postLaborRequest(
+    const agent_index::Component&                indexComp,
+    goods_supplier::Component&                   goodsSupplier,
+    labor_demander::Component&                   laborDemander,
+    tbb::concurrent_vector<world::LaborRequest>& requestBox
+) {
+    const int  id{indexComp.getId()};
+    const bool isSold{goods_supplier::isSold({goodsSupplier})};
+    labor_demander::postJob(id, isSold, requestBox, {laborDemander}, laborDemander);
+}
+}  // namespace orchestrator
