833fbc94396f730df996086f6e800f6ea021198a
yahagikimuthi <yahagikimuth@gmail.com>
2026-07-10 04:45:00 +0000

    savePath追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
-#include <iostream>
-
 #include "core/engine.hpp"
 
 auto main() -> int {
-    core::Engine engine{3};
-    std::cout << "compile is success!\n";
+    std::string  savePath{"../result"};
+    core::Engine engine{3, savePath};
 }
