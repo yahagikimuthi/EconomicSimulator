97bca0b10621cad6118723dfc336af65f6c98656
yahagikimuthi <yahagikimuth@gmail.com>
2026-07-14 06:47:18 +0000

    求人者のソートをインデックスではなくreference_wrapperを用いたものに変更

--- src/strategies/labor_demander/offer.cpp
+++ src/strategies/labor_demander/offer.cpp
@@ -3,28 +3,31 @@
 #include <tbb/concurrent_vector.h>
 #include <algorithm>
 #include <cstddef>
-#include <numeric>
+#include <functional>
 
 #include "world/message.hpp"
 
 namespace labor_demander {
 namespace {
 void sortApplicants(
-    const int                                        employ,
-    std::vector<std::size_t>&                        sortApplicantIdxs,
-    const tbb::concurrent_vector<world::LaborEntry>& entryBox
+    const int                                               employ,
+    std::vector<std::reference_wrapper<world::LaborEntry>>& applicants,
+    tbb::concurrent_vector<world::LaborEntry>&              entryBox
 ) {
     const std::size_t k{std::min(entryBox.size(), static_cast<std::size_t>(employ))};
-    sortApplicantIdxs.resize(entryBox.size());
-    std::ranges::iota(sortApplicantIdxs, 0UZ);
+    applicants.clear();
+    for (world::LaborEntry& entry : entryBox) applicants.emplace_back(std::ref(entry));
     const bool isOver{entryBox.size() > static_cast<std::size_t>(employ)};
+
     if (not isOver) return;
 
     std::ranges::nth_element(
-        sortApplicantIdxs,
-        sortApplicantIdxs.begin() + static_cast<int>(k),
+        applicants,
+        applicants.begin() + static_cast<int>(k),
         std::ranges::greater{},
-        [&entryBox](const std::size_t idx) -> double { return entryBox[idx].productPower_; }
+        [](const std::reference_wrapper<const world::LaborEntry> entryRef) -> double {
+            return entryRef.get().productPower_;
+        }
     );
 }
 }  // namespace
@@ -39,14 +42,15 @@ void offerApplicants(OfferApplicantsView view) {
 
     const int employ{view.employPlan()};
 
-    static thread_local std::vector<std::size_t> sortApplicantIdxs;
-    sortApplicants(employ, sortApplicantIdxs, myRequest.entryBox_);
+    static thread_local std::vector<std::reference_wrapper<world::LaborEntry>> applicants;
+    sortApplicants(employ, applicants, myRequest.entryBox_);
 
     int offerNum{};
-    for (const std::size_t i : sortApplicantIdxs) {
+    for (auto requestRef : applicants) {
+        world::LaborEntry& request = requestRef.get();
         if (offerNum >= employ) break;
-        myRequest.entryBox_[i].isOffer_ = true;
-        view.recordOffer(myRequest.entryBox_[i]);
+        request.isOffer_ = true;
+        view.recordOffer(request);
         ++offerNum;
     }
 }
