23e327025b80da670d0db5b2867088b3a3afd45a
yahagikimuthi <yahagikimuth@gmail.com>
2026-07-16 07:21:56 +0000

    parquet形式でdataframeを保存できるよう修正

--- scripts/main.py
+++ scripts/main.py
@@ -3,9 +3,12 @@ import plot
 from typing import Final
 import pandas as pd
 
+def readParquet(filename: str) -> pd.DataFrame:
+    return pd.read_parquet(filename)
+
 def main() -> None:
-    metricsDf: Final[pd.DataFrame] = load.loadMetrics("outputs/metrics.h5")
-    
+    metricsDf: Final[pd.DataFrame] = readParquet("outputs/metricsDf.parquet")
+
     plot.saveSingleLinePlot(x=metricsDf["step"].iloc[5000:], y=metricsDf["avgFirmAssets"].iloc[5000:], title="Firm Asset", xlabel="step", ylabel="asset",
                             color="red", filename="outputs/firm.png")
     plot.saveSingleLinePlot(x=metricsDf["step"].iloc[5000:], y=metricsDf["avgHholdAssets"].iloc[5000:], title="Household Asset", xlabel="step", ylabel="asset",
