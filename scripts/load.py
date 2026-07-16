0d275674a2bfb409887db3bf8636983c9f36a527
yahagikimuthi <yahagikimuth@gmail.com>
2026-07-16 06:59:55 +0000

    pythonはすべてのデータをロードする

--- scripts/load.py
+++ scripts/load.py
@@ -4,20 +4,32 @@ import numpy as np
 from typing import Final
 
 def loadSimulationResults(filepath: str) -> None:
-    allRows = []
+    allRows: list = []
 
     with h5py.File(filepath, "r") as f:
         for stepKey in sorted(f.keys()):
             stepNum: Final[int] = int(stepKey.split("_")[1])
-            group: Final = f[stepKey]
+            group: Final[h5py.Group] = f[stepKey]
 
+            assets: Final[np.array] = group["firmAssets"][:]
+            postedEmployments: Final[np.array] = group["postedEmployments"][:]
+            employments: Final[np.array] = group["employments"][:]
             prices: Final[np.array] = group["prices"][:]
+            supplies: Final[np.array] = group["supplies"][:]
+            markups: Final[np.array] = group["markups"][:]
+            inventories: Final[np.array] = group["inventories"]
 
             for firmId in range(len(prices)):
                 allRows.append({
                     "step": stepNum,
                     "firmId": firmId,
-                    "prices": prices[firmId]
+                    "assets": assets[firmId],
+                    "postedEmployments": postedEmployments[firmId],
+                    "employments": employments[firmId],
+                    "prices": prices[firmId],
+                    "supplies": supplies[firmId],
+                    "markups": markups[firmId],
+                    "inventories": inventories[firmId]
                 })
     
     print("生データを読み込みました")
@@ -25,6 +37,6 @@ def loadSimulationResults(filepath: str) -> None:
 
 def loadMetrics(filepath: str) -> None:
     with h5py.File(filepath, "r") as f:
-        data = {key: f[key][:] for key in f.keys()}
+        data: Final[dict] = {key: f[key][:] for key in f.keys()}
     print("統計量を読み込みました")
     return pd.DataFrame(data)
