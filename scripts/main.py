476a356cbe918bef71999c7f15af1826a1e38ff1
yahagikimuthi <yahagikimuth@gmail.com>
2026-07-16 09:03:00 +0000

    統計量追加

--- scripts/main.py
+++ scripts/main.py
@@ -7,12 +7,13 @@ def readParquet(filename: str) -> pd.DataFrame:
     return pd.read_parquet(filename)
 
 def main() -> None:
-    metricsDf: Final[pd.DataFrame] = readParquet("outputs/metricsDf.parquet")
+    metricsDf: Final[pd.DataFrame] = load.loadMetrics("outputs/metrics.h5")
 
-    plot.saveSingleLinePlot(x=metricsDf["step"].iloc[5000:], y=metricsDf["avgFirmAssets"].iloc[5000:], title="Firm Asset", xlabel="step", ylabel="asset",
-                            color="red", filename="outputs/firm.png")
-    plot.saveSingleLinePlot(x=metricsDf["step"].iloc[5000:], y=metricsDf["avgHholdAssets"].iloc[5000:], title="Household Asset", xlabel="step", ylabel="asset",
-                            color="red", filename="outputs/hhold.png")
+    plot.saveSingleLinePlot(x=metricsDf["step"].iloc[600:], y=metricsDf["avgFirmAssets"].iloc[600:], title="Firm Asset", xlabel="step", ylabel="asset", color="red", filename="outputs/firm.png")
+    plot.saveSingleLinePlot(x=metricsDf["step"].iloc[600:], y=metricsDf["avgHholdAssets"].iloc[600:], title="Household Asset", xlabel="step", ylabel="asset", color="red", filename="outputs/hhold.png")
+    
+    plot.saveSingleLinePlot(x=metricsDf["step"].iloc[600:], y=metricsDf["avgPrices"].iloc[600:],
+    title="cpi", xlabel="step", ylabel="price", color="red", filename="outputs/price.png")
 
 if __name__ == "__main__":
     main()
