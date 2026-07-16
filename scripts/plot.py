a7de17e35e19a841e31a0afb893237939c886067
yahagikimuthi <yahagikimuth@gmail.com>
2026-07-16 09:03:19 +0000

    gifに関してupdate関数をクロージャーで定義

--- scripts/plot.py
+++ scripts/plot.py
@@ -19,29 +19,15 @@ def saveSingleLinePlot(x, y, title: str, xlabel: str, ylabel: str, color: str, f
     plt.close()
     print(f"グラフを保存しました: {filename}")
 
-def genericKdeFrame(frame, df: pd.DataFrame, columnName: str, ax, line, title: str, titleTemplate, xEval):
-    stepData: Final[pd.DataFrame] = df[df["step"] == frame][columnName].values
-
-    if len(stepData) > 1 and np.var(stepData) > 1e-5:
-        kde: Final = gaussian_kde(stepData)
-        yVals: Final = kde(xEval)
-    else:
-        yVals = np.zeros_like(xEval)
-        if len(stepData) > 0:
-            idx: Final = (np.abs(xEval - np.mean(stepData))).argmin()
-            yVals[idx] = 1.0
-
-    line.set_ydata(yVals)
-    title.set_text(titleTemplate.format(frame=frame))
-    return line, title
-
 def generateKdeGif(df: pd.DataFrame, columnName: str, filename: str, titleTemplate: str ="Distribution Dynamics [{frame}]", 
-                   xlabel="Value", color="teal", stepInterval=2):
+                   xlabel: str ="Value", color: str ="teal", stepInterval: int =2) -> None:
     fig, ax = plt.subplots(figsize=(7, 4))
     
     xMin: Final[float] = df[columnName].min()
     xMax: Final[float] = df[columnName].max()
     xEval: Final = np.linspace(xMin, xMax, 200)
+
+    ax.set_ylim(0, 0.01)
     
     ax.set_xlabel(xlabel)
     ax.set_ylabel("Density")
@@ -49,21 +35,26 @@ def generateKdeGif(df: pd.DataFrame, columnName: str, filename: str, titleTempla
     
     line, = ax.plot(xEval, np.zeros_like(xEval), color=color, lw=2)
     title = ax.set_title("")
-    
-    boundUpdate = partial(
-        genericKdeFrame, 
-        df=df, 
-        columnName=columnName, 
-        ax=ax, 
-        line=line, 
-        title=title, 
-        titleTemplate=titleTemplate, 
-        xEval=xEval
-    )
+
+    def update(frame):
+        stepData: Final[pd.DataFrame] = df[df["step"] == frame][columnName].values
+
+        if len(stepData) > 1 and np.var(stepData) > 1e-5:
+            kde: Final = gaussian_kde(stepData)
+            yVals: Final = kde(xEval)
+        else:
+            yVals = np.zeros_like(xEval)
+            if len(stepData) > 0:
+                idx: Final = (np.abs(xEval - np.mean(stepData))).argmin()
+                yVals[idx] = 1.0
+
+        line.set_ydata(yVals)
+        title.set_text(titleTemplate.format(frame=frame))
+        return line, title
     
     maxStep: Final[float] = df['step'].max()
-    ani: Final = animation.FuncAnimation(
-        fig, boundUpdate, frames=range(0, maxStep + 1, stepInterval), blit=True
+    ani: Final[animation.FuncAnimation] = animation.FuncAnimation(
+        fig, update, frames=range(0, maxStep + 1, stepInterval), blit=True
     )
     
     ani.save(filename, writer='pillow', fps=15)
