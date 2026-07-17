c5c56cc96d66993568a552000e82e11e98ded708
yahagikimuthi <yahagikimuth@gmail.com>
2026-07-17 00:59:47 +0000

    Viewを無名名前空間に移動

--- src/strategies/goods_supplier/posting.cpp
+++ src/strategies/goods_supplier/posting.cpp
@@ -11,38 +11,6 @@
 #include "world/message.hpp"
 
 namespace goods_supplier {
-struct CalcSupplyView final : BaseView<Component> {
-    using BaseView<Component>::BaseView;
-
-    auto firmProductPower() const -> double { return comp_.production_.firmProductPower_; }
-    auto sumEmployeeProductPower() const -> double {
-        return comp_.production_.sumEmployeeProductPower_;
-    }
-    auto inventory() const -> double { return comp_.production_.inventory_; }
-};
-
-struct [[nodiscard]] CalcMarkupView final : BaseView<Component> {
-    using BaseView<Component>::BaseView;
-
-    auto markupAdjustVol() const -> double { return comp_.parameter_.markupAdjustmentVolatility_; }
-    auto lastMarkup() const -> double { return comp_.log_.markup_; }
-    auto isSold() const -> bool { return comp_.log_.isSold_; }
-    auto rng() -> pcg32& { return comp_.rng_; }
-};
-
-struct [[nodiscard]] JudgePriceView final : BaseView<Component> {
-    using BaseView<Component>::BaseView;
-};
-
-struct [[nodiscard]] CalcAvgCostView final : BaseView<Component> {
-    using BaseView<Component>::BaseView;
-
-    auto firmProductPower() const -> double { return comp_.production_.firmProductPower_; }
-    auto sumEmployeeProductPower() const -> double {
-        return comp_.production_.sumEmployeeProductPower_;
-    }
-};
-
 namespace {
 [[nodiscard]] auto markupGuard(
     const double markup, const double epsilon = config::goods_supplier::epsilonMarkup
@@ -56,17 +24,45 @@ namespace {
     return std::max(price, epsilon);
 }
 
+struct CalcSupplyView final : BaseView<Component> {
+    using BaseView<Component>::BaseView;
+
+    auto firmProductPower() const -> double { return comp_.production_.firmProductPower_; }
+    auto sumEmployeeProductPower() const -> double {
+        return comp_.production_.sumEmployeeProductPower_;
+    }
+    auto inventory() const -> double { return comp_.production_.inventory_; }
+};
+
 [[nodiscard]] auto calcSupply(const CalcSupplyView& view) -> double {
     const double out{(view.firmProductPower() * view.sumEmployeeProductPower()) + view.inventory()};
     assert(out >= 0.0 && "supply amount is required >= 0");
     return out;
 }
 
+struct [[nodiscard]] CalcMarkupView final : BaseView<Component> {
+    using BaseView<Component>::BaseView;
+
+    auto markupAdjustVol() const -> double { return comp_.parameter_.markupAdjustmentVolatility_; }
+    auto lastMarkup() const -> double { return comp_.log_.markup_; }
+    auto isSold() const -> bool { return comp_.log_.isSold_; }
+    auto rng() -> pcg32& { return comp_.rng_; }
+};
+
 [[nodiscard]] auto calcMarkup(CalcMarkupView view) -> double {
     const double alpha{std::abs(helper::randNormal(view.rng(), 0.0, view.markupAdjustVol()))};
     const double nextMarkup{view.lastMarkup() + (view.isSold() ? alpha : -alpha)};
     return markupGuard(nextMarkup);
 }
+
+struct [[nodiscard]] CalcAvgCostView final : BaseView<Component> {
+    using BaseView<Component>::BaseView;
+
+    auto firmProductPower() const -> double { return comp_.production_.firmProductPower_; }
+    auto sumEmployeeProductPower() const -> double {
+        return comp_.production_.sumEmployeeProductPower_;
+    }
+};
 
 [[nodiscard]] auto calcAvgCost(const CalcAvgCostView& view, const double totalCost) -> double {
     assert(totalCost >= 0.0 && "total cost is required > 0");
@@ -76,6 +72,10 @@ namespace {
     assert(avgCost >= 0.0 && "average cost is required >= 0");
     return avgCost;
 }
+
+struct [[nodiscard]] JudgePriceView final : BaseView<Component> {
+    using BaseView<Component>::BaseView;
+};
 
 [[nodiscard]] auto judgePrice(JudgePriceView view, const double markup, const double totalCost)
     -> double {
