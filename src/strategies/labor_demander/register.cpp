4f0609515f0f4f430ea2496e7747feb7a0227f54
yahagikimuthi <yahagikimuth@gmail.com>
2026-07-22 00:58:09 +0000

    名簿登録時に求人票にrosterEntryポインタを渡す

--- src/strategies/labor_demander/register.cpp
+++ src/strategies/labor_demander/register.cpp
@@ -7,16 +7,19 @@
 namespace labor_demander {
 [[nodiscard]] auto registerMember(RegisterMemberView view) -> double {
     if (not view.isPosting()) return 0.0;
-    const auto& myRequest = view.myRequest();
+    auto& myRequest = view.myRequest();
     view.applicantNumPlus(myRequest.entryBox_.size());
 
     double sumProductPower{};
     int    employeeCnt{};
+    auto&  myBoard = view.myCompanyBoard();
+    auto&  roster  = myBoard.roster_;
     for (const auto i : std::views::iota(0UZ, view.offerNum())) {
-        const auto& entry = view.offerApplicant(i);
+        auto& entry = view.offerApplicant(i);
         if (not entry.isAccept_) continue;
         sumProductPower += entry.productPower_;
         ++employeeCnt;
+        entry.rosterEntry_ = &roster.emplace_back(myRequest.wage_, myBoard);
     }
 
     view.updateLedger(myRequest.wage_, employeeCnt);
