0b8dd024afcacf3b69a8ec32e33686aa2905f8c6
yahagikimuthi <yahagikimuth@gmail.com>
2026-08-22 16:25:16 +0000

    家計は現在資産に基づいて購買を決定する。賃金はすでに資産として評価されている。

--- include/orchestrator/consumer_goods.hpp
+++ include/orchestrator/consumer_goods.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <tbb/concurrent_vector.h>
-
 #include "components/common.hpp"
 #include "components/consumer_goods_demander.hpp"
 #include "components/consumer_goods_supplier/consumer_goods_supplier.hpp"
@@ -9,7 +7,6 @@
 #include "components/labor_supplier/labor_supplier.hpp"
 #include "core/values/common.hpp"
 #include "world/common.hpp"
-#include "world/goods.hpp"
 
 namespace abm::consumer_goods {
 void product(LaborSupplier& laborSupplier, const Market phase) noexcept {
@@ -27,11 +24,10 @@ void postGoods(
 void purchase(
     const HHoldFinance&    finance,
     ConsumerGoodsDemander& goodsDemander,
-    const LaborSupplier&   laborSupplier,
     ConsumerGoodsMarket&   market,
     const Step             step
 ) noexcept {
-    goodsDemander.request(finance.asset() + laborSupplier.wage(), step, market);
+    goodsDemander.request(finance.asset(), step, market);
 }
 
 void trade(ConsumerGoodsSupplier& goodsSupplier) noexcept { goodsSupplier.trade(); }
