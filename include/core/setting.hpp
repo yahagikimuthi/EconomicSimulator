0f0ef943a0ec66bf71c7a2bd7b63d685860e6f86
yahagikimuthi <yahagikimuth@gmail.com>
2026-08-23 18:01:26 +0000

    エージェント数を削減

--- include/core/setting.hpp
+++ include/core/setting.hpp
@@ -84,7 +84,7 @@ constexpr RandomParameter mpc            = UniformParameter<double>{.min = 0.5, 
 namespace abm::setting::agent_count {
 constexpr auto bToCFirm = 10;
 constexpr auto bToBFirm = 10;
-constexpr auto hhold    = 10000;
+constexpr auto hhold    = 100;
 }  // namespace abm::setting::agent_count
 
 namespace abm::setting::agent_finance {
