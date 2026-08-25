a56deba9d41a4532ac62b117905f085110683f9d
yahagikimuthi <yahagikimuth@gmail.com>
2026-08-25 02:21:11 +0000

    内部リンク解決のため定数にinlineを付与

--- include/core/setting.hpp
+++ include/core/setting.hpp
@@ -27,80 +27,87 @@ using RandomParameter =
 }  // namespace abm
 
 namespace abm::setting {
-constexpr auto useRuntimeRandomSeed       = false;
-constexpr auto fixedSeedState             = std::uint64_t{0x853c49e6748fea9bULL};
-constexpr auto fixedSeedStream            = std::uint64_t{0xda3e39cb94b95bdbULL};
-constexpr auto simulationResultOutputPath = "../outputs/result.h5"sv;
-constexpr auto metricDataOutputPath       = "../outputs/metrics.h5"sv;
+inline constexpr auto useRuntimeRandomSeed       = false;
+inline constexpr auto fixedSeedState             = std::uint64_t{0x853c49e6748fea9bULL};
+inline constexpr auto fixedSeedStream            = std::uint64_t{0xda3e39cb94b95bdbULL};
+inline constexpr auto simulationResultOutputPath = "../outputs/result.h5"sv;
+inline constexpr auto metricDataOutputPath       = "../outputs/metrics.h5"sv;
 }  // namespace abm::setting
 
 namespace abm::labor::demander::setting {
-constexpr RandomParameter lastApplicants     = UniformParameter<int>{.min = 10, .limit = 20};
-constexpr RandomParameter lastEmployPlan     = UniformParameter<int>{.min = 10, .limit = 20};
-constexpr RandomParameter offerRate          = UniformParameter<double>{.min = 0.0, .limit = 1.0};
-constexpr RandomParameter offerRateAdjustVol = UniformParameter<double>{.min = 0.1, .limit = 0.2};
-constexpr RandomParameter lastWage      = UniformParameter<double>{.min = 10.0, .limit = 100.0};
-constexpr RandomParameter wageAdjustVol = UniformParameter<double>{.min = 0.001, .limit = 0.005};
+inline constexpr RandomParameter lastApplicants = UniformParameter<int>{.min = 10, .limit = 20};
+inline constexpr RandomParameter lastEmployPlan = UniformParameter<int>{.min = 10, .limit = 20};
+inline constexpr RandomParameter offerRate = UniformParameter<double>{.min = 0.0, .limit = 1.0};
+inline constexpr RandomParameter offerRateAdjustVol =
+    UniformParameter<double>{.min = 0.1, .limit = 0.2};
+inline constexpr RandomParameter lastWage = UniformParameter<double>{.min = 10.0, .limit = 100.0};
+inline constexpr RandomParameter wageAdjustVol =
+    UniformParameter<double>{.min = 0.001, .limit = 0.005};
 }  // namespace abm::labor::demander::setting
 
 namespace abm::labor::supplier::setting {
-constexpr RandomParameter productPower =
+inline constexpr RandomParameter productPower =
     NormalParameter{.mean = 1.0, .dev = 1.0 / 3.0, .min = 0.01, .max = 2.0};
-constexpr auto            jobSampleCnt = 2;
-constexpr auto            jobEntryCnt  = 2;
-constexpr RandomParameter changingJobThreshold =
+inline constexpr auto            jobSampleCnt = 2;
+inline constexpr auto            jobEntryCnt  = 2;
+inline constexpr RandomParameter changingJobThreshold =
     UniformParameter<double>{.min = 0.01, .limit = 0.1};
 }  // namespace abm::labor::supplier::setting
 
 namespace abm::base_goods::supplier::setting {
-constexpr RandomParameter lastDemand     = UniformParameter<double>{.min = 10.0, .limit = 100.0};
-constexpr RandomParameter demandForecast = UniformParameter<double>{.min = 10.0, .limit = 100.0};
-constexpr RandomParameter demandForecastAdjustVol =
+inline constexpr RandomParameter lastDemand = UniformParameter<double>{.min = 10.0, .limit = 100.0};
+inline constexpr RandomParameter demandForecast =
+    UniformParameter<double>{.min = 10.0, .limit = 100.0};
+inline constexpr RandomParameter demandForecastAdjustVol =
     UniformParameter<double>{.min = 0.1, .limit = 0.2};
-constexpr RandomParameter lastSupply      = UniformParameter<double>{.min = 10.0, .limit = 20.0};
-constexpr RandomParameter lastSalesAmount = UniformParameter<double>{.min = 10.0, .limit = 20.0};
-constexpr RandomParameter desiredEmploy   = UniformParameter<int>{.min = 10, .limit = 20};
-constexpr RandomParameter lastMarkup      = UniformParameter<double>{.min = 0.1, .limit = 0.2};
-constexpr RandomParameter markupAdjustVol = UniformParameter<double>{.min = 0.1, .limit = 0.2};
-constexpr RandomParameter priceAdjustVol  = UniformParameter<double>{.min = 0.1, .limit = 0.2};
-constexpr RandomParameter targetInvRatio  = UniformParameter<double>{.min = 0.1, .limit = 0.2};
-constexpr RandomParameter productPower    = UniformParameter<double>{.min = 0.01, .limit = 2.0};
-constexpr RandomParameter producerGoodsEfficiency =
+inline constexpr RandomParameter lastSupply = UniformParameter<double>{.min = 10.0, .limit = 20.0};
+inline constexpr RandomParameter lastSalesAmount =
+    UniformParameter<double>{.min = 10.0, .limit = 20.0};
+inline constexpr RandomParameter desiredEmploy = UniformParameter<int>{.min = 10, .limit = 20};
+inline constexpr RandomParameter lastMarkup    = UniformParameter<double>{.min = 0.1, .limit = 0.2};
+inline constexpr RandomParameter markupAdjustVol =
+    UniformParameter<double>{.min = 0.1, .limit = 0.2};
+inline constexpr RandomParameter priceAdjustVol =
+    UniformParameter<double>{.min = 0.1, .limit = 0.2};
+inline constexpr RandomParameter targetInvRatio =
+    UniformParameter<double>{.min = 0.1, .limit = 0.2};
+inline constexpr RandomParameter productPower = UniformParameter<double>{.min = 0.01, .limit = 2.0};
+inline constexpr RandomParameter producerGoodsEfficiency =
     UniformParameter<double>{.min = 1.0, .limit = 10.0};
-constexpr RandomParameter inventory = UniformParameter<double>{.min = 10.0, .limit = 100.0};
+inline constexpr RandomParameter inventory = UniformParameter<double>{.min = 10.0, .limit = 100.0};
 }  // namespace abm::base_goods::supplier::setting
 
 namespace abm::consumer_goods::demander::setting {
-constexpr auto            maxPurchaseFrequency = 1;
-constexpr auto            goodsSampleCnt       = 2;
-constexpr RandomParameter mpc                  = UniformParameter<double>{.min = 0.5, .limit = 0.8};
+inline constexpr auto            maxPurchaseFrequency = 1;
+inline constexpr auto            goodsSampleCnt       = 2;
+inline constexpr RandomParameter mpc = UniformParameter<double>{.min = 0.5, .limit = 0.8};
 }  // namespace abm::consumer_goods::demander::setting
 
 namespace abm::production_goods::demander::setting {
-constexpr auto goodsSampleCnt = 2;
+inline constexpr auto goodsSampleCnt = 2;
 }  // namespace abm::production_goods::demander::setting
 
 namespace abm::setting::agent_count {
-constexpr auto bToCFirm = 10;
-constexpr auto bToBFirm = 100;
-constexpr auto hhold    = 10000;
+inline constexpr auto bToCFirm = 10;
+inline constexpr auto bToBFirm = 100;
+inline constexpr auto hhold    = 10000;
 }  // namespace abm::setting::agent_count
 
 namespace abm::setting::agent_finance {
-constexpr RandomParameter firm  = UniformParameter<double>{.min = 100.0, .limit = 1000.0};
-constexpr RandomParameter hhold = UniformParameter<double>{.min = 10.0, .limit = 100.0};
+inline constexpr RandomParameter firm  = UniformParameter<double>{.min = 100.0, .limit = 1000.0};
+inline constexpr RandomParameter hhold = UniformParameter<double>{.min = 10.0, .limit = 100.0};
 }  // namespace abm::setting::agent_finance
 
 namespace abm::setting::save_name {
-constexpr auto firmAssets        = "firmAssets"sv;
-constexpr auto postedEmployments = "postedEmployments"sv;
-constexpr auto postedWages       = "postedWages"sv;
-constexpr auto employments       = "employments"sv;
-constexpr auto sumWages          = "sumWages"sv;
-constexpr auto prices            = "prices"sv;
-constexpr auto supplies          = "supplies"sv;
-constexpr auto markups           = "markups"sv;
-constexpr auto inventories       = "inventories"sv;
-constexpr auto householdAssets   = "householdAssets"sv;
-constexpr auto wages             = "wages"sv;
+inline constexpr auto firmAssets        = "firmAssets"sv;
+inline constexpr auto postedEmployments = "postedEmployments"sv;
+inline constexpr auto postedWages       = "postedWages"sv;
+inline constexpr auto employments       = "employments"sv;
+inline constexpr auto sumWages          = "sumWages"sv;
+inline constexpr auto prices            = "prices"sv;
+inline constexpr auto supplies          = "supplies"sv;
+inline constexpr auto markups           = "markups"sv;
+inline constexpr auto inventories       = "inventories"sv;
+inline constexpr auto householdAssets   = "householdAssets"sv;
+inline constexpr auto wages             = "wages"sv;
 }  // namespace abm::setting::save_name
