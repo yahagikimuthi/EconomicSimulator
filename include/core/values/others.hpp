b978fbe0dacf2a83c1ca3c18b76832e30f94d326
yahagikimuthi <yahagikimuth@gmail.com>
2026-08-25 21:11:44 +0000

    TaxRateオブジェクトを追加

--- include/core/values/others.hpp
+++ include/core/values/others.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
+#include "core/assertion.hpp"
 #include "core/values/common.hpp"
 
 namespace abm {
-class TaxRate : public value_object::ValueObjectMixin<TaxRate> {
+class TaxRate : public value_object::BaseValueObjectMixin<double> {
   public:
     [[nodiscard]] explicit constexpr TaxRate(const double value)
-        : ValueObjectMixin<TaxRate>(value) {}
+        : BaseValueObjectMixin<double>(value) {
+        ASSERT(0.0 <= value and value <= 1.0);
+    }
 };
 }  // namespace abm
