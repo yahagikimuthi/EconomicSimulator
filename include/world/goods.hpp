3866cfebd400da06260080cb6e2f8e23a4fb4ce2
yahagikimuthi <yahagikimuth@gmail.com>
2026-08-25 23:16:32 +0000

    workspaceのコンストラクタ定義をinlineに書く

--- include/world/goods.hpp
+++ include/world/goods.hpp
@@ -17,11 +17,26 @@ namespace abm {
 class Workspace final {
   public:
     [[nodiscard]] constexpr Workspace() noexcept = default;
-    constexpr ~Workspace() noexcept              = default;
-    constexpr Workspace(const Workspace& other) noexcept;
-    constexpr auto operator=(const Workspace& other) noexcept -> Workspace&;
-    constexpr Workspace(Workspace&& other) noexcept;
-    constexpr auto operator=(Workspace&& other) noexcept -> Workspace&;
+    ~Workspace() noexcept                        = default;
+    [[nodiscard]] Workspace(const Workspace& other) noexcept
+        : totalInput_{other.totalInput_.load()} {}
+    auto operator=(const Workspace& other) noexcept -> Workspace& {
+        if (this == &other) return *this;
+        const auto input = other.totalInput_.load();
+        totalInput_.store(input);
+        return *this;
+    }
+    [[nodiscard]] Workspace(Workspace&& other) noexcept : totalInput_{other.totalInput_.load()} {
+        other.totalInput_.store(0.0);
+    }
+    auto operator=(Workspace&& other) noexcept -> Workspace& {
+        if (this == &other) return *this;
+        const double input{other.totalInput_.load()};
+        totalInput_.store(input);
+
+        other.totalInput_.store(0.0);
+        return *this;
+    }
 
     void addInput(const double workerProductPower) noexcept {
         ASSERT(workerProductPower > 0.0);
@@ -35,26 +50,6 @@ class Workspace final {
   private:
     std::atomic<double> totalInput_;
 };
-
-constexpr Workspace::Workspace(const Workspace& other) noexcept
-    : totalInput_{other.totalInput_.load()} {}
-constexpr auto Workspace::operator=(const Workspace& other) noexcept -> Workspace& {
-    if (this == &other) return *this;
-    const double input{other.totalInput_.load()};
-    totalInput_.store(input);
-    return *this;
-}
-constexpr Workspace::Workspace(Workspace&& other) noexcept : totalInput_{other.totalInput_.load()} {
-    other.totalInput_.store(0.0);
-}
-constexpr auto Workspace::operator=(Workspace&& other) noexcept -> Workspace& {
-    if (this == &other) return *this;
-    const double input{other.totalInput_.load()};
-    totalInput_.store(input);
-
-    other.totalInput_.store(0.0);
-    return *this;
-}
 
 class ConsumerGoodsRequest final {
   public:
