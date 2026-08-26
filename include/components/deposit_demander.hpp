622fa773c8002d4313197eb21702746fd21f09a9
yahagikimuthi <yahagikimuth@gmail.com>
2026-08-26 15:53:04 +0000

    預金需要者コンポーネントのロジックをほとんど確定

--- include/components/deposit_demander.hpp
+++ include/components/deposit_demander.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <deque>
 #include <optional>
+#include <span>
 #include <vector>
 
 #include "core/util.hpp"
@@ -12,16 +14,42 @@ namespace abm::deposit::demander {
 class DepositDemander {
     using Market  = DepositMarket;
     using Request = DepositRequest;
+    using Entry   = DepositEntry;
 
   public:
     [[nodiscard]] explicit constexpr DepositDemander(RandomGenerator& masterRng) noexcept;
 
-    void postRequest(const AgentID id, Market& market) noexcept;
+    void postRequest(const AgentID id, Market& market) noexcept {
+        myRequest_ = market.request(id, interestRate_);
+    }
+
+    void accountOpening() noexcept {
+        if (not isPosting()) return;
+        auto entries = packEntry();
+        for (Entry& entry : entries) {
+            auto& account = savingAccounts_.emplace_back(entry.entryAgentId);
+            entry.account(account);
+        }
+    }
+
+    void endStep() noexcept { reset(); }
 
   private:
+    [[nodiscard]] auto isPosting() const noexcept -> bool { return myRequest_.has_value(); }
+
+    [[nodiscard]] auto packEntry() noexcept -> std::span<RefWrap<Entry>> {
+        ASSERT(isPosting());
+        static thread_local auto entries = std::vector<RefWrap<Entry>>{};
+        entries.clear();
+        myRequest_->packEntry(entries);
+        return entries;
+    }
+
+    void reset() noexcept { myRequest_.reset(); }
+
     std::optional<DepositRequest&> myRequest_;
     InterestRate                   interestRate_;
-    std::vector<SavingAccount>     savingAccount_;
+    std::deque<DepositAccount>     savingAccounts_;
 };
 }  // namespace abm::deposit::demander
 
