913340b26bc83b89a69dc0945561abe373468ded
yahagikimuthi <yahagikimuth@gmail.com>
2026-08-27 16:50:01 +0000

    deposit_supplierを簡素化

--- include/components/finance/deposit_supplier.hpp
+++ include/components/finance/deposit_supplier.hpp
@@ -2,16 +2,19 @@
 
 #include <optional>
 
-#include "core/setting.hpp"
-#include "core/util.hpp"
 #include "core/values/common.hpp"
 #include "core/values/others.hpp"
 #include "world/deposit.hpp"
 
 namespace abm::deposit::supplier {
-class AccountManager final {
+
+class DepositSupplier final {
+    using Entry   = DepositEntry;
+    using Request = DepositRequest;
+    using Market  = DepositMarket;
+
   public:
-    [[nodiscard]] constexpr AccountManager() noexcept = default;
+    [[nodiscard]] constexpr DepositSupplier() noexcept = default;
 
     [[nodiscard]] auto balance() const noexcept -> Deposit {
         return account_.transform(&DepositAccount::balance).value_or(Deposit{0.0});
@@ -21,40 +24,8 @@ class AccountManager final {
 
   private:
     [[nodiscard]] auto haveAccount() const noexcept -> bool { return account_.has_value(); }
+
     std::optional<DepositAccount&> account_{std::nullopt};
-};
-
-class DepositSupplier final {
-    using Entry   = DepositEntry;
-    using Request = DepositRequest;
-    using Market  = DepositMarket;
-
-  public:
-    [[nodiscard]] explicit constexpr DepositSupplier(RandomGenerator& masterRng) noexcept;
-
-    void requestAccountOpening(
-        const AgentID id, Market& market, const int sampleCnt = setting::maxSampleCnt
-    ) noexcept {
-        auto picked = market.pickRequest(id, rng_, sampleCnt);
-        if (not picked) return;
-        myEntry_ = picked->entry(id);
-    }
-
-    void registerAccount() noexcept {
-        if (not isPosting()) return;
-        accountManager_.setAccount(myEntry_->account());
-    }
-
-    void endStep() noexcept { reset(); }
-
-  private:
-    [[nodiscard]] auto isPosting() const noexcept -> bool { return myEntry_.has_value(); }
-
-    void reset() noexcept { myEntry_.reset(); }
-
-    mutable RandomGenerator rng_;
-    AccountManager          accountManager_;
-    std::optional<Entry&>   myEntry_{std::nullopt};
 };
 }  // namespace abm::deposit::supplier
 
