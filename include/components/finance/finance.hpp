f3cfc429569e394129af2054f13bf7a3828a0715
yahagikimuthi <yahagikimuth@gmail.com>
2026-08-31 02:36:51 +0000

    PLを追加 資産減額処理を追加

--- include/components/finance/finance.hpp
+++ include/components/finance/finance.hpp
@@ -29,37 +29,74 @@ class BankRegistry final {
     std::vector<Bank> banks_;
 };
 
+enum class AccountItem : char { Sales, CapitalGoodsCost, Depreciation, Taxes };
+
+struct PL {
+    void reset() noexcept { sales = capitalGoodsCost = depreciation = taxes = Money{0.0}; }
+
+    Money sales{0.0};
+    Money capitalGoodsCost{0.0};
+    Money depreciation{0.0};
+    Money taxes{0.0};
+};
+
+struct BS {};
+
+enum class Color : char { Red, Blue, Yellow };
+
 class Finance {
   public:
     explicit Finance(const Money asset, RandomGenerator& masterRng) noexcept
         : cash_{asset}, cashRatio_{masterRng.random(setting::cashRatio)} {}
 
-    void assetPlus(const Money add) noexcept {
-        ASSERT(add >= Money{0.0});
+    [[nodiscard]] auto withdraw(const Money sub, const AccountItem item) noexcept -> Money {
+        ASSERT(sub.isZeroOrMore());
+        ASSERT(item != AccountItem::Sales);
+        if (currentCashRatio() > cashRatio_) {
+            const auto withdraw = depositSupplier_.tryWithdraw(sub);
+            ASSERT(withdraw <= sub);
+            const auto cashOut = sub - withdraw;
+            cash_ -= cashOut;
+            postToPl(withdraw + cashOut, item);
+            return withdraw + cashOut;
+        }
+        const auto cashOut = std::min(cash_, sub);
+        cash_ -= cashOut;
+        const auto rest        = sub - cashOut;
+        const auto withdraw    = depositSupplier_.tryWithdraw(rest);
+        const auto moreCashOut = rest - withdraw;
+        cash_ -= moreCashOut;
+        postToPl(cashOut + withdraw + moreCashOut, item);
+        return cashOut + withdraw + moreCashOut;
+    }
 
+    void assetPlus(const Money add, const AccountItem item) noexcept {
+        ASSERT(add >= Money{0.0});
+        ASSERT(item == AccountItem::Sales);
+
+        pl_.sales += add;
         // 現金比率が目標以上で、預金に成功した場合早期リターン
         if (currentCashRatio() > cashRatio_ and depositSupplier_.tryDeposit(add)) return;
         cash_ += add;
     }
 
-    [[nodiscard]] auto claimBudget(const Money claim) noexcept -> Money {
+    [[nodiscard]] auto claimBudget(const Money claim) const noexcept -> Money {
         ASSERT(claim >= Money{0.0});
         ASSERT(asset() >= Money{0.0});
 
-        if (currentCashRatio() <= cashRatio_) {
-            const auto out = std::min(cash_, claim);
-            cash_ -= out;
-            return out;
+        if (currentCashRatio() > cashRatio_) {
+            const auto cashOut  = std::min(cash_, claim);
+            const auto rest     = claim - cashOut;
+            const auto withdraw = std::min(static_cast<Money>(depositSupplier_.balance()), rest);
+            return cashOut + withdraw;
         }
 
-        const auto withdraw = depositSupplier_.tryWithdraw(claim);
+        const auto withdraw = std::min(static_cast<Money>(depositSupplier_.balance()), claim);
         ASSERT(withdraw <= claim);
 
         const auto rest = claim - withdraw;
         ASSERT(cash_ >= Money{0.0});
         const auto cashOut = std::min(cash_, rest);
-        cash_ -= cashOut;
-
         return withdraw + cashOut;
     }
 
@@ -75,6 +112,26 @@ class Finance {
     }
 
   private:
+    void postToPl(const Money money, const AccountItem item) noexcept {
+        ASSERT(money.isZeroOrMore());
+        switch (item) {
+            case AccountItem::Sales:
+                pl_.sales += money;
+                break;
+            case AccountItem::CapitalGoodsCost:
+                pl_.capitalGoodsCost += money;
+                break;
+            case AccountItem::Depreciation:
+                pl_.depreciation += money;
+                break;
+            case AccountItem::Taxes:
+                pl_.taxes += money;
+                break;
+            default:
+        }
+    }
+
+    PL              pl_;
     DepositSupplier depositSupplier_;
     Money           cash_;
     const double    cashRatio_;
