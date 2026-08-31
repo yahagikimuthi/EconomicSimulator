efa151d4100edfec0cf73bd4577c4654f1aef225
yahagikimuthi <yahagikimuth@gmail.com>
2026-08-31 06:13:13 +0000

    trader.trade()を決定

--- include/components/capital_supplier/trader.hpp
+++ include/components/capital_supplier/trader.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+#include <functional>
 #include <optional>
 #include <pcg_random.hpp>
 #include <span>
@@ -7,6 +9,7 @@
 
 #include "components/base_goods_supplier/common.hpp"
 #include "components/base_goods_supplier/ledger.hpp"
+#include "components/finance/finance.hpp"
 #include "others/util.hpp"
 #include "values/goods.hpp"
 #include "world/capital.hpp"
@@ -30,14 +33,13 @@ class Trader final {
         ledger_.makeNewPage(plan.supply);
     }
 
-    void trade() noexcept {
+    void trade(FirmFinance& finance) noexcept {
         if (not isPosting()) return;
-        auto       requestBox = requestBoxRef();
-        const auto demand     = myEntry_->totalDemand();
+        const auto demand = calcTotalDemand();
         if (demand.isZero()) return;
         const auto tradeAmount    = ledger_.tradableAmount(demand);
         const auto isExcessDemand = ledger_.isExcessDemand(demand);
-        isExcessDemand ? performRationedTrade(requestBox) : myEntry_->performFullTrade();
+        isExcessDemand ? performRationedTrade(finance) : performFullTrade(finance);
         ledger_.readResult({.price = myEntry_->price, .demand = demand, .salesAmount = tradeAmount}
         );
     }
@@ -54,30 +56,52 @@ class Trader final {
     }
 
   private:
-    [[nodiscard]] auto requestBoxRef() noexcept -> std::span<RefWrap<Request>> {
-        ASSERT(isPosting());
-        static thread_local auto requestBox = std::vector<RefWrap<Request>>{};
-        requestBox.clear();
-        myEntry_->packRequest(requestBox);
-        return requestBox;
+    [[nodiscard]] auto calcTotalDemand() const noexcept -> GoodsQuantity {
+        const auto requests = myEntry_->requests();
+        return std::ranges::fold_left(
+            requests | std::views::transform([this](const Request& req) -> GoodsQuantity {
+                return req.payment() / myEntry_->price;
+            }),
+            GoodsQuantity{0.0},
+            std::plus{}
+        );
     }
 
-    void performRationedTrade(std::span<RefWrap<Request>> requestBox) noexcept {
-        rng_.shuffle(requestBox);
+    void performRationedTrade(FirmFinance& finance) noexcept {
+        auto requests = packRequest();
+        rng_.shuffle(requests);
+
         auto remainAmount = ledger_.inventory();
-        for (RefWrap<Request> reqRef : requestBox) {
+        for (auto reqRef : requests) {
             auto&      req       = reqRef.get();
-            const auto reqAmount = req.amount;
+            const auto reqAmount = req.payment() / myEntry_->price;
             if (remainAmount <= reqAmount) {
-                req.trade(remainAmount);
+                const auto sales = req.trade(remainAmount);
+                finance.assetPlus(sales, FirmFinance::AccountItem::Sales);
                 return;
             }
-            req.trade(reqAmount);
+            const auto sales = req.trade(reqAmount);
+            finance.assetPlus(sales, FirmFinance::AccountItem::Sales);
             remainAmount -= reqAmount;
         }
     }
 
     [[nodiscard]] auto isPosting() const noexcept -> bool { return myEntry_.has_value(); }
+
+    [[nodiscard]] auto packRequest() noexcept -> std::span<RefWrap<Request>> {
+        static thread_local auto refs = std::vector<RefWrap<Request>>{};
+        refs.clear();
+        for (auto& req : myEntry_->requests()) refs.emplace_back(std::ref(req));
+        return refs;
+    }
+
+    void performFullTrade(FirmFinance& finance) noexcept {
+        for (auto& request : myEntry_->requests()) {
+            const auto tradeAmount = request.payment() / myEntry_->price;
+            const auto sales       = request.trade(tradeAmount);
+            finance.assetPlus(sales, FirmFinance::AccountItem::Sales);
+        }
+    }
 
     Ledger                  ledger_;
     std::optional<Entry&>   myEntry_{std::nullopt};
