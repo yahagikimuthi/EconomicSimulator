0ef4a0049f6a126e699b7997ff145bc7208c2d2a
yahagikimuthi <yahagikimuth@gmail.com>
2026-08-31 10:53:25 +0000

    Ledgerを削除

--- include/components/capital_demander.hpp
+++ include/components/capital_demander.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <algorithm>
-#include <concepts>
 #include <limits>
 #include <optional>
 #include <pcg_random.hpp>
@@ -20,49 +19,6 @@ using Request = base_goods::Request;
 struct Log final {
     const Money         purchase{std::numeric_limits<double>::epsilon()};
     const GoodsQuantity tradeAmount{std::numeric_limits<double>::infinity()};
-};
-
-struct ATradeResult final {
-    const Price         price;
-    const GoodsQuantity tradeAmount;
-};
-
-struct TradeResult final {
-    const Money         purchased;
-    const GoodsQuantity tradeAmount;
-};
-
-class Ledger final {
-  public:
-    Ledger() noexcept = default;
-
-    void readTradeResult(const ATradeResult& result) noexcept {
-        ASSERT(result.price.isZeroOrMore());
-        ASSERT(result.tradeAmount.isZeroOrMore());
-
-        purchasing_ += result.price * result.tradeAmount;
-        purchaseAmount_ += result.tradeAmount;
-    }
-
-    void reset() noexcept {
-        ASSERT(purchasing_ >= Money{0.0});
-        ASSERT(purchaseAmount_.isZeroOrMore());
-        purchasing_     = Money{0.0};
-        purchaseAmount_ = GoodsQuantity{0.0};
-    }
-
-    [[nodiscard]] auto publishTradeResult() const noexcept -> TradeResult {
-        return {.purchased = purchasing_, .tradeAmount = purchaseAmount_};
-    }
-
-  private:
-    Money         purchasing_{0.0};
-    GoodsQuantity purchaseAmount_{0.0};
-};
-
-template <typename F>
-concept ReadResultFn = requires(F f, const TradeResult& result) {
-    { f(result) } -> std::same_as<void>;
 };
 
 class CapitalDemander final {
@@ -103,7 +59,6 @@ class CapitalDemander final {
     }
 
   private:
-    Ledger                        ledger_;
     RandomGenerator               rng_;
     Log                           log_;
     std::optional<GoodsQuantity>  purchaseAmountPlan_{std::nullopt};
