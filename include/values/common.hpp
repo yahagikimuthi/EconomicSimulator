50dbef6678396b14561fb92bb2f80e6e9f44f98a
yahagikimuthi <yahagikimuth@gmail.com>
2026-08-31 07:22:46 +0000

    AgentID::operator++を削除

--- include/values/common.hpp
+++ include/values/common.hpp
@@ -22,11 +22,6 @@ class Money final : public value_object::BaseValueObject<double>,
 class AgentID final : public value_object::BaseValueObject<int>,
                       public value_object::CompareMixin<AgentID> {
   public:
-    auto operator++() noexcept -> AgentID& {
-        ++value_;
-        return *this;
-    }
-
     explicit constexpr AgentID(const int value) noexcept : BaseValueObject<int>(value) {}
 };
 
