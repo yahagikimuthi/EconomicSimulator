652cc978572a9c4c6ce2470141387184e0063442
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-01 01:18:16 +0000

    適切なオーバーロードを追加

--- include/system/finance.hpp
+++ include/system/finance.hpp
@@ -15,10 +15,24 @@
 
 namespace abm::finance::detail {
 [[nodiscard]] inline auto laborDemanderAnnualRequestBudget(
+    LaborDemander& laborDemander, CapitalSupplier& goodsSupplier
+) noexcept -> Budget {
+    const auto adjust = goodsSupplier.calcDesiredEmploy(laborDemander.employeeCnt());
+    return laborDemander.planAnnualAndRequestBudget(adjust, goodsSupplier.salesForecast());
+}
+
+[[nodiscard]] inline auto laborDemanderAnnualRequestBudget(
     LaborDemander& laborDemander, GoodsSupplier& goodsSupplier
 ) noexcept -> Budget {
     const auto adjust = goodsSupplier.calcDesiredEmploy(laborDemander.employeeCnt());
     return laborDemander.planAnnualAndRequestBudget(adjust, goodsSupplier.salesForecast());
+}
+
+[[nodiscard]] inline auto capitalDemanderRequestBudget(
+    CapitalDemander& capitalDemander, CapitalSupplier& goodsSupplier
+) noexcept -> Budget {
+    const auto desired = goodsSupplier.requiresCapital();
+    return capitalDemander.planAndRequestBudget(desired);
 }
 
 [[nodiscard]] inline auto capitalDemanderRequestBudget(
@@ -44,16 +58,33 @@ inline void distributeAnnualBudget(
     laborDemander.reviseAnnualPlan(laborDemanderRequest);
     capitalDemander.revisePlan(laborDemanderRequest - budget);
 }
-
-[[nodiscard]] auto capitalDemanderRequestBudget(
-    GoodsSupplier& goodsSupplier, CapitalDemander& capitalDemander
-) noexcept -> Budget {
-    const auto desired = goodsSupplier.requiresCapital();
-    return capitalDemander.planAndRequestBudget(desired);
-}
 }  // namespace abm::finance::detail
 
 namespace abm::finance {
+inline void judgeYearlyAndMonthlyBudget(
+    FirmFinance&     finance,
+    LaborDemander&   laborDemander,
+    CapitalDemander& capitalDemander,
+    CapitalSupplier& goodsSupplier
+) noexcept {
+    const auto salesForecast = goodsSupplier.planAndExpectSales(laborDemander.sumWage());
+    const auto laborDemanderRequest =
+        detail::laborDemanderAnnualRequestBudget(laborDemander, goodsSupplier);
+    const auto capitalDemanderRequest =
+        detail::capitalDemanderRequestBudget(capitalDemander, goodsSupplier);
+    const auto total = laborDemanderRequest + capitalDemanderRequest - salesForecast;
+    if (total.isZeroOrLess()) {
+        laborDemander.reviseAnnualPlan(laborDemanderRequest);
+        capitalDemander.revisePlan(capitalDemanderRequest);
+        return;
+    }
+    const auto budget = finance.claimBudget(total) + salesForecast;
+    ASSERT(budget <= laborDemanderRequest + capitalDemanderRequest);
+    detail::distributeAnnualBudget(
+        laborDemander, capitalDemander, budget, laborDemanderRequest, capitalDemanderRequest
+    );
+}
+
 inline void judgeYearlyAndMonthlyBudget(
     FirmFinance&     finance,
     LaborDemander&   laborDemander,
@@ -98,50 +129,7 @@ inline void judgeMonthlyBudget(
     capitalDemander.revisePlan(std::max(budget - sumWage, Budget{0.0}));
 }
 
-inline void judgeYearlyAndMonthlyBudget(
-    FirmFinance&     finance,
-    LaborDemander&   laborDemander,
-    GoodsSupplier&   goodsSupplier,
-    CapitalDemander& capitalDemander
-) noexcept {
-    const auto salesForecast = goodsSupplier.planAndExpectSales(laborDemander.sumWage());
-    const auto laborDemanderRequest =
-        detail::laborDemanderAnnualRequestBudget(laborDemander, goodsSupplier);
-    const auto capitalDemanderRequest =
-        detail::capitalDemanderRequestBudget(goodsSupplier, capitalDemander);
-    const auto total = laborDemanderRequest + capitalDemanderRequest - salesForecast;
-    if (total.isZeroOrLess()) {
-        laborDemander.reviseAnnualPlan(laborDemanderRequest);
-        capitalDemander.revisePlan(capitalDemanderRequest);
-        return;
-    }
-    const auto budget = finance.claimBudget(total) + salesForecast;
-    ASSERT(budget <= total);
-    detail::distributeAnnualBudget(
-        laborDemander, capitalDemander, budget, laborDemanderRequest, capitalDemanderRequest
-    );
-}
-
-inline void judgeMonthlyBudget(
-    FirmFinance&     finance,
-    LaborDemander&   laborDemander,
-    GoodsSupplier&   goodsSupplier,
-    CapitalDemander& capitalDemander
-) noexcept {
-    const auto salesForecast = goodsSupplier.planAndExpectSales(laborDemander.sumWage());
-    const auto sumWage       = static_cast<Budget>(laborDemander.sumWage());
-    const auto capitalDemanderRequest =
-        detail::capitalDemanderRequestBudget(goodsSupplier, capitalDemander);
-    const auto total = sumWage + capitalDemanderRequest - salesForecast;
-    if (total.isZeroOrLess()) {
-        capitalDemander.revisePlan(capitalDemanderRequest);
-        return;
-    }
-    const auto budget = finance.claimBudget(total) + salesForecast;
-    ASSERT(budget <= total);
-    capitalDemander.revisePlan(std::max(budget - sumWage, Budget{0.0}));
-}
-
+// 家計用
 inline void judgeMonthlyBudget(
     HHoldFinance& finance, LaborSupplier& laborSupplier, GoodsDemander& goodsDemander
 ) noexcept {
