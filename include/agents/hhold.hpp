e1e36795a305fc6365864459159002b6b34279f8
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-01 12:50:12 +0000

    家計の処理を追加

--- include/agents/hhold.hpp
+++ include/agents/hhold.hpp
@@ -15,14 +15,46 @@ class HHold final {
 
   public:
     HHold(const AgentID id, RandomGenerator& masterRng) noexcept
-        : finance_{id, masterRng}, laborSupplier_{masterRng}, goodsDemander_{masterRng}, id_{id} {}
+        : finance_{id, masterRng}, labor_{masterRng}, goods_{masterRng}, id_{id} {}
 
-    void act(const Date& date, Markets& markets) noexcept;
+    void act(const Date& date, Markets& markets) noexcept {
+        labor_.product();
+        const auto day = date.day();
+        if (day == operationDay_)
+            actOperationDay(date.month(), markets);
+        else if (day == operationDay_ + 1)
+            actAfterOperationDay();
+    }
 
   private:
+    void actOperationDay(const int month, Markets& markets) noexcept {
+        const auto wage         = static_cast<Budget>(labor_.wage());
+        const auto asset        = static_cast<Budget>(finance_.asset());
+        const auto purchasePlan = goods_.planAndRequestBudget(asset + wage);
+        const auto total        = purchasePlan - wage;
+        if (total.isZeroOrLess()) {
+            goods_.revisePlan(purchasePlan);
+            return;
+        }
+        const auto budget = finance_.claimBudget(total) + wage;
+        ASSERT(budget <= total + wage);
+        goods_.revisePlan(budget);
+
+        if (month == 2)
+            labor_.entry(id_, markets.laborMarket);
+        else if (month == 4)
+            labor_.accept();
+        else if (month == 5)
+            labor_.recordRosterEntry();
+
+        goods_.request(id_, finance_, markets.goodsMarket);
+    }
+
+    void actAfterOperationDay() noexcept { goods_.afterTrade(finance_); }
+
     HHoldFinance  finance_;
-    LaborSupplier laborSupplier_;
-    GoodsDemander goodsDemander_;
+    LaborSupplier labor_;
+    GoodsDemander goods_;
     const AgentID id_;
     const int     operationDay_{instanceCnt++ % setting::dayInMonth};
 };
