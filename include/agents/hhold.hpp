9badfa3420822f7538c447ee3afff0028856b285
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-01 12:38:57 +0000

    operationDayを追加

--- include/agents/hhold.hpp
+++ include/agents/hhold.hpp
@@ -4,22 +4,26 @@
 #include "components/finance/others_finance.hpp"
 #include "components/goods_demander.hpp"
 #include "components/labor_supplier/labor_supplier.hpp"
+#include "others/setting.hpp"
 #include "others/util.hpp"
 #include "values/common.hpp"
 #include "values/date.hpp"
 
 namespace abm {
 class HHold final {
+    static inline constinit int instanceCnt{};
+
   public:
     HHold(const AgentID id, RandomGenerator& masterRng) noexcept
         : finance_{id, masterRng}, laborSupplier_{masterRng}, goodsDemander_{masterRng}, id_{id} {}
 
-    void act(const Date today, Markets& markets) noexcept;
+    void act(const Date& date, Markets& markets) noexcept;
 
   private:
     HHoldFinance  finance_;
     LaborSupplier laborSupplier_;
     GoodsDemander goodsDemander_;
     const AgentID id_;
+    const int     operationDay_{instanceCnt++ % setting::dayInMonth};
 };
 }  // namespace abm
