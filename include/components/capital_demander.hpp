8011cf2a995e599e549ef1f77e5bdb3ee6ff26ed
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-01 12:13:26 +0000

    メソッド名を修正

--- include/components/capital_demander.hpp
+++ include/components/capital_demander.hpp
@@ -23,7 +23,7 @@ class CapitalDemander final {
     explicit CapitalDemander(RandomGenerator& masterRng) noexcept
         : rng_{{masterRng.makeUint64(), masterRng.makeUint64()}} {}
 
-    [[nodiscard]] auto planAndRequestBudget(const GoodsQuantity desiredAmount) noexcept -> Budget {
+    [[nodiscard]] auto requestBudget(const GoodsQuantity desiredAmount) noexcept -> Budget {
         const auto avgPrice = log_.purchase / log_.tradeAmount;
         purchaseAmountPlan_ = desiredAmount;
         budget_             = static_cast<Budget>(avgPrice * desiredAmount);
