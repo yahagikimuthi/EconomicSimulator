e5b93329b3ca3de8ac103f67fc63b7fd1b34f05a
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-01 05:35:34 +0000

    Agentの呼び出し引数を決定

---
+++ include/agents/common.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "values/date.hpp"
+#include "world/base_goods.hpp"
+#include "world/labor.hpp"
+
+namespace abm {
+struct Markets final {
+    BaseGoodsMarket& capitalMarket;
+    BaseGoodsMarket& goodsMarket;
+    LaborMarket&     laborMarket;
+};
+
+template <typename T>
+concept IAgent = requires(T t, const Date& today, Markets& markets) { t.act(today, markets); };
+}  // namespace abm
