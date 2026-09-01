b0dc8d5a68bb6557d9c45114f15c2467ebef2576
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-01 05:57:00 +0000

    初期化処理を追加

--- include/agents/capital_firm.hpp
+++ include/agents/capital_firm.hpp
@@ -5,6 +5,7 @@
 #include "components/capital_supplier/capital_supplier.hpp"
 #include "components/finance/firm_finance.hpp"
 #include "components/labor_demander/labor_demander.hpp"
+#include "others/setting.hpp"
 #include "others/util.hpp"
 #include "values/common.hpp"
 #include "values/date.hpp"
@@ -14,7 +15,12 @@ class CapitalFirm final {
     static inline constinit int instanceCnt{};
 
   public:
-    CapitalFirm(const AgentID id, RandomGenerator& masterRng) noexcept;
+    CapitalFirm(const AgentID id, RandomGenerator& masterRng) noexcept
+        : finance_{id, masterRng},
+          laborDemander_{id, masterRng},
+          capitalDemander_{masterRng},
+          capitalSupplier_{masterRng},
+          id_{id} {}
 
     void act(const Date& today, Markets& markets) noexcept;
 
@@ -24,6 +30,6 @@ class CapitalFirm final {
     CapitalDemander capitalDemander_;
     CapitalSupplier capitalSupplier_;
     const AgentID   id_;
-    const int       businessDay_;
+    const int       businessDay_{instanceCnt++ % setting::dayInMonth};
 };
 }  // namespace abm
