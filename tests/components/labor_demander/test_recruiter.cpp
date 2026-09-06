44f1ec54bebfd007a7dba82ce15a3c3039acb99e
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-06 14:03:32 +0000

    test_recruiter.hppを追加

---
+++ tests/components/labor_demander/test_recruiter.cpp
@@ -0,0 +1,7 @@
+#include "components/labor_demander/recruiter.hpp"
+
+#include "doctest.h"
+
+namespace abm::labor::demander::recruiter {
+namespace {}  // namespace
+}  // namespace abm::labor::demander::recruiter
