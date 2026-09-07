e8ee9c75f1703a76cdec8758f7f870d644632811
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-07 01:57:45 +0000

    オファー数が0のとき、Marketにポストしない

--- tests/components/labor_demander/test_recruiter.cpp
+++ tests/components/labor_demander/test_recruiter.cpp
@@ -1,7 +1,32 @@
 #include "components/labor_demander/recruiter.hpp"
 
+#include <inplace_vector>
+
+#include "components/labor_demander/common.hpp"
 #include "doctest.h"
+#include "others/util.hpp"
+#include "tests/util.hpp"
+#include "values/common.hpp"
+#include "values/labor.hpp"
+#include "world/labor.hpp"
 
 namespace abm::labor::demander::recruiter {
-namespace {}  // namespace
+namespace {
+TEST_CASE("Recruiterのテスト") {  // NOLINT
+    auto recruiter = Recruiter{};
+    auto market    = Market{};
+    auto rng       = makeRng();
+    auto out       = std::inplace_vector<RefWrap<Request>, 1UZ>{};
+
+    SUBCASE("オファー数が0の場合、Marketにポストしない") {
+        const auto plan =
+            RecruitPlan{.wage = Wage{10.0}, .employ = HeadCount{0.0}, .offer = HeadCount{0.0}};
+        recruiter.post(AgentID{42}, plan, market);
+
+        market.pickRequest(AgentID{101}, out, rng);
+
+        CHECK(out.empty());
+    }
+}
+}  // namespace
 }  // namespace abm::labor::demander::recruiter
