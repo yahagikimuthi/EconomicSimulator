52702b3534643c75255b50c70bd3f959896cd09a
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-07 05:54:26 +0000

    予算が満額回答の場合のテストも追加

--- tests/components/labor_demander/test_human_resource.cpp
+++ tests/components/labor_demander/test_human_resource.cpp
@@ -72,6 +72,60 @@ TEST_CASE("HumanResourceのテスト") {  // NOLINT
         CHECK(hr.employeeCnt().value() == 3);
         CHECK(hr.sumWage().value() == doctest::Approx(sumWage));
     }
+
+    SUBCASE("予算が満額回答の場合、結果は変わらない") {
+        SUBCASE("全員解雇の場合、概算要求はゼロ、解雇の場合、名簿はすべて無効化") {
+            const auto reqBudget = hr.planAndRequestBudget(HeadCount{3});
+            CHECK(reqBudget.isZero());
+            CHECK(reqBudget == hr.requestedBudget());
+
+            hr.revisePlan(reqBudget);
+
+            hr.layOffs();
+
+            CHECK(not roster1.isOccupied());
+            CHECK(not roster2.isOccupied());
+            CHECK(not roster3.isOccupied());
+
+            CHECK(hr.sumWage().isZero());
+            CHECK(hr.employeeCnt().isZero());
+        }
+
+        SUBCASE("一部解雇の場合、総賃金から平均賃金*解雇数を除いた額を概算要求し、名簿が一部無効化"
+        ) {
+            const auto reqBudget = hr.planAndRequestBudget(HeadCount{2});
+            CHECK(reqBudget.value() == doctest::Approx(hr.sumWage().value() - (avgWage * 2)));
+            CHECK(reqBudget == hr.requestedBudget());
+
+            hr.revisePlan(reqBudget);
+
+            hr.layOffs();
+
+            CHECK(not roster1.isOccupied());
+            CHECK(not roster2.isOccupied());
+            CHECK(roster3.isOccupied());
+
+            CHECK(hr.employeeCnt().value() == 1);
+            CHECK(hr.sumWage().value() == doctest::Approx(303));
+        }
+
+        SUBCASE("誰も解雇しない場合、総賃金を概算要求、名簿はすべて有効") {
+            const auto reqBudget = hr.planAndRequestBudget(HeadCount{0});
+            CHECK(reqBudget.value() == doctest::Approx(hr.sumWage().value()));
+            CHECK(reqBudget == hr.requestedBudget());
+
+            hr.revisePlan(reqBudget);
+
+            hr.layOffs();
+
+            CHECK(roster1.isOccupied());
+            CHECK(roster2.isOccupied());
+            CHECK(roster3.isOccupied());
+
+            CHECK(hr.employeeCnt().value() == 3);
+            CHECK(hr.sumWage().value() == doctest::Approx(sumWage));
+        }
+    }
 }
 }  // namespace
 }  // namespace abm::labor::demander
