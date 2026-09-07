54abab9a0dcc112f495b51860c9a41758ec4f6fe
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-07 07:36:23 +0000

    JobHunterのテストファイルを追加

---
+++ tests/components/labor_supplier/test_job_hunter.cpp
@@ -0,0 +1,10 @@
+#include "components/labor_supplier/job_hunter.hpp"
+
+#include "doctest.h"
+
+namespace abm::labor::supplier {
+namespace {
+TEST_CASE("JobHunterのテスト") {  // NOLINT
+}
+}  // namespace
+}  // namespace abm::labor::supplier
