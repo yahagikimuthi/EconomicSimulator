f69aaae07970149e3069fda3989cfa0994215b2f
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-07 02:31:19 +0000

    オファー数が0でないとき、Marketから発見可能であることのテスト追加

--- tests/components/labor_demander/test_recruiter.cpp
+++ tests/components/labor_demander/test_recruiter.cpp
@@ -23,7 +23,7 @@ TEST_CASE("Recruiterのテスト") {  // NOLINT
     auto space     = base_goods::Workspace{};
 
     SUBCASE("オファー数が0の場合") {
-        const auto plan =
+        constexpr auto plan =
             RecruitPlan{.wage = Wage{10.0}, .employ = HeadCount{0.0}, .offer = HeadCount{0.0}};
         recruiter.post(AgentID{42}, plan, market);
 
@@ -39,6 +39,21 @@ TEST_CASE("Recruiterのテスト") {  // NOLINT
 
             CHECK(result.applicants.isZero());
             CHECK(result.employ.isZero());
+        }
+    }
+
+    SUBCASE("オファー数が0でない場合") {
+        constexpr auto plan =
+            RecruitPlan{.wage = Wage{10.0}, .employ = HeadCount{3.0}, .offer = HeadCount{5.0}};
+        recruiter.post(AgentID{42}, plan, market);
+
+        SUBCASE("Marketからサンプリングすることが可能で、IDと賃金が等しい") {
+            market.pickRequest(AgentID{101}, out, rng);
+
+            CHECK(out.size() == 1UZ);
+            const auto& sample = out[0].get();
+            CHECK(sample.firmID == AgentID{42});
+            CHECK(sample.wage == Wage{10.0});
         }
     }
 }
