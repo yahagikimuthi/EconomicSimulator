de9c9491330bef8dd478ea55ad6dc53d3e9b6fb8
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-09 10:54:17 +0000

    Governmentは銀行口座を持たない

--- include/components/finance/others_finance.hpp
+++ include/components/finance/others_finance.hpp
@@ -77,50 +77,28 @@ class HHoldFinance final {
 
 class GovernmentFinance final {
   public:
-    explicit GovernmentFinance(RandomGenerator& masterRng) noexcept
-        : cashRatio_{masterRng.random(setting::cashRatio)} {}
+    explicit GovernmentFinance() noexcept = default;
 
     [[nodiscard]] auto tryWithdraw(const Budget tryingWithdraw) noexcept -> Money {
         assert(tryingWithdraw.isZeroOrMore());
         const auto sub = Money{tryingWithdraw.value()};
-        if (currentCashRatio() > cashRatio_) {
-            const auto withdraw = bankAccount_.withdraw(sub);
-            assert(withdraw <= sub);
-            const auto cashOut = sub - withdraw;
-            cash_ -= cashOut;
-            return withdraw + cashOut;
-        }
-        const auto cashOut = std::min(cash_, sub);
-        cash_ -= cashOut;
-        const auto rest        = sub - cashOut;
-        const auto withdraw    = bankAccount_.withdraw(rest);
-        const auto moreCashOut = rest - withdraw;
-        cash_ -= moreCashOut;
-        return cashOut + withdraw + moreCashOut;
+        const auto out = std::min(sub, cash_);
+        cash_ -= out;
+
+        const auto moreOut = sub - out;
+        cash_ -= moreOut;
+        return sub;
     }
 
     void deposit(const Money add) noexcept {
         assert(add.isZeroOrMore());
-        if (currentCashRatio() > cashRatio_) {
-            bankAccount_.deposit(add);
-            return;
-        }
         cash_ += add;
     }
 
-    [[nodiscard]] auto asset() const noexcept -> Budget {
-        return static_cast<Budget>(cash_) + bankAccount_.balance();
-    }
+    [[nodiscard]] auto asset() const noexcept -> Budget { return static_cast<Budget>(cash_); }
 
   private:
-    [[nodiscard]] auto currentCashRatio() const noexcept -> double {
-        if (asset().isZero()) return 0.0;
-        return static_cast<Budget>(cash_) / asset();
-    }
-
-    BankAccount  bankAccount_{AgentID{-1}};
-    Money        cash_{0.0};
-    const double cashRatio_;
+    Money cash_{0.0};
 };
 }  // namespace abm::finance
 
