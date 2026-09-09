85b0f65751d2aa544c66109e980bd9cc48d52a88
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-09 08:36:51 +0000

    Cacheを削除

--- include/components/base_goods_supplier/common.hpp
+++ include/components/base_goods_supplier/common.hpp
@@ -42,24 +42,6 @@ class Memory final {
     std::optional<T> next_{std::nullopt};
 };
 
-template <typename T>
-class Cache final {
-  public:
-    explicit Cache(const T cache) noexcept : cache_{cache} {}
-    [[nodiscard]] auto cache() const noexcept -> T { return cache_; }
-
-    void reset() noexcept {
-        if (not next_) return;
-        cache_ = *next_;
-        next_.reset();
-    }
-    void next(const T next) noexcept { next_ = next; }
-
-  private:
-    T                cache_;
-    std::optional<T> next_{std::nullopt};
-};
-
 class CentralMemory final {
   public:
     explicit CentralMemory() noexcept = default;
