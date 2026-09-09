e1983360fac60f650b46eb3dedf6b6241949dc9e
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-09 06:32:23 +0000

    stepをmonthに変更

--- include/engine/logger.hpp
+++ include/engine/logger.hpp
@@ -30,9 +30,9 @@ class Logger final {
 
     [[nodiscard]] auto isValid() const noexcept -> bool { return file_.isValid(); }
 
-    void save(const CensusDropBox& dropBox, const int step) noexcept {
+    void save(const CensusDropBox& dropBox, const int month) noexcept {
         namespace name = global_setting::save_name;
-        auto groupPath = std::string{"/step_" + std::to_string(step)};
+        auto groupPath = std::string{"/step_" + std::to_string(month)};
         auto group     = HighFive::Group{file_.createGroup(groupPath)};
 
         auto create =
