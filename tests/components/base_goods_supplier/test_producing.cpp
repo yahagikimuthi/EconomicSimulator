fdfe5e2f32f45d56b3820ddb51ea8cd27be9d1c3
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-11 11:57:42 +0000

    Producerのリファクタに伴いテストケースを再考する

--- tests/components/base_goods_supplier/test_producing.cpp
+++ tests/components/base_goods_supplier/test_producing.cpp
@@ -1,15 +1,9 @@
 #include "components/base_goods_supplier/produsing.hpp"
 
 #include "doctest.h"
-#include "tests/util.hpp"
 
 namespace abm::base_goods::supplier {
 namespace {
-TEST_CASE("Producerのテスト") {  // NOLINT
-    auto rng      = makeRng();
-    auto producer = Producer{rng};
-
-    SUBCASE("デフォルトで供給量はゼロ") { CHECK(producer.produce().isZero()); }
-}
+TEST_CASE("Producerのテスト") {}
 }  // namespace
 }  // namespace abm::base_goods::supplier
