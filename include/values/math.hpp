5bc0a0f36226a4ac6bbc877ec8c3c6d20553cc15
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-11 10:53:18 +0000

    値オブジェト用のpowを追加

--- include/values/math.hpp
+++ include/values/math.hpp
@@ -15,4 +15,9 @@ template <value_object::ComputableObject T>
 [[nodiscard]] constexpr auto ceil(const T a) noexcept -> T {
     return T{std::ceil(a.value())};
 }
+
+template <value_object::ComputableObject T>
+[[nodiscard]] constexpr auto pow(const T x, const double y) noexcept -> T {
+    return T{std::pow(x.value(), y)};
+}
 }  // namespace abm
