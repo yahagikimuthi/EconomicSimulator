c27d55984184f503fbd1cdc04c5788efcedc6c29
yahagikimuthi <yahagikimuthi@gmail.com>
2026-09-15 10:56:05 +0000

    環境構築

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import {EconomicChart} from './components/EconomicChart';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Economic Simulator Dashboard</h1>
+      <EconomicChart />
+    </div>
+  )
+}
+
+export default App
